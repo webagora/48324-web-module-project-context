c1f89cdc72f69f7bc97cdbdb66915771107646b2
RFNoC <rongjun-geng@rfnocs-mac-pro.lan>
2021-11-04 20:03:39 -0700

    STEP 3 - Providing data with ProductContext

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import React, { useState } from 'react';
+import React, { useState, createContext } from 'react';
 import { Route } from 'react-router-dom';
 import data from './data';
 
@@ -6,6 +6,8 @@ import data from './data';
 import Navigation from './components/Navigation';
 import Products from './components/Products';
 import ShoppingCart from './components/ShoppingCart';
+
+import ProductContext from './contexts/ProductContext';
 
 function App() {
 	const [products] = useState(data);
@@ -21,9 +23,11 @@ function App() {
 			<Navigation cart={cart} />
 
 			{/* Routes */}
+			<ProductContext.Provider value={{ products, addItem }}>
 			<Route exact path="/">
-				<Products products={products} addItem={addItem} />
+				<Products  />
 			</Route>
+			</ProductContext.Provider>
 
 			<Route path="/cart">
 				<ShoppingCart cart={cart} />
