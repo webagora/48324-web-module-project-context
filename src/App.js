da666c95c1f733c583b79b6096fd01c6a603e660
RFNoC <rongjun-geng@rfnocs-mac-pro.lan>
2021-11-04 21:01:33 -0700

    STEP 7 - The final stretch

--- src/App.js
+++ src/App.js
@@ -23,7 +23,7 @@ function App() {
 		<div className="App">
 			<ProductContext.Provider value={{ products, addItem }}>
 				<CartContext.Provider value={cart}>
-					<Navigation cart={cart} />
+					<Navigation />
 
 					{/* Routes */}
 					
